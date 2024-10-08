7def2be4bff3f634ced0caea01f81af0370b5f5d
aakritigkmit <aakriti@gkmit.co>
2024-10-08 18:02:30 +0530

    2nd commit

---
+++ gitfile.js
@@ -0,0 +1,16 @@
+I made a folder added some file
+and pushed into Git
+then locally delete the file 
+but didnot remove the folder just rename kiya and then used but wo git ka origin same hi aarha tha jaaha phle push kia tha folder 
+
+Command used to find origin
+
+
+git remote -v
+
+showed:
+origin	https://github.com/aakritigkmit/InternAssignment.git (fetch)
+origin	https://github.com/aakritigkmit/InternAssignment.git (push)
+
+delete Origin
+git remote remove origin
